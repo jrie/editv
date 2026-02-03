b300139aac6873acf0569605ce94b51befeab7d2
nimrag-b <124409677+nimrag-b@users.noreply.github.com>
2026-02-03 06:56:59 +0000

    rewrote and edited to make it functional and add functionality
    Also implemented my own generic interface for open/save files

--- editv/interface.c
+++ editv/interface.c
@@ -4,6 +4,7 @@
 #ifdef _WIN32
 
 #include <windows.h>
+#include <shobjidl_core.h>
 #include "SDL3/SDL_video.h"
 
 
@@ -19,11 +20,139 @@ void edv_init(SDL_Window *window) {
 
 }
 
-const VCHAR* edv_open_file(edv_file_options* opt) {
+int edv_open_file(const char* lastOpenFile, const char* buffer, size_t length) {
+
+	OPENFILENAMEA ofn;       // common dialog box structure
+	char szFile[260];       // buffer for file name
+	HWND hwnd = 0;              // owner window
+	HANDLE hf;              // file handle
+
+	// Initialize OPENFILENAME
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = hwnd;
+	ofn.lpstrFile = szFile;
+	// Set lpstrFile[0] to '\0' so that GetOpenFileName does not 
+	// use the contents of szFile to initialize itself.
+	ofn.lpstrFile[0] = '\0';
+	ofn.nMaxFile = sizeof(szFile);
+	ofn.lpstrFilter = "All\0*.*\0Text\0*.TXT\0";
+	ofn.nFilterIndex = 1;
+
+	ofn.nMaxFileTitle = 0;
+
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+
+	if (lastOpenFile != NULL) {
+
+		char dirBuf[256];
+		char filenameBuf[256];
+		char extBuf[256];
+
+		//todo - error handling
+		_splitpath_s(lastOpenFile, NULL, 0, dirBuf, 256, filenameBuf, 256, extBuf, 256);
+
+		
+		if ((strlen(filenameBuf) + strlen(extBuf)) > 256) {
+			//file buffer too big
+			return NULL;
+		}
+
+		wchar_t *res = strcat(filenameBuf, extBuf);
+
+		ofn.lpstrFileTitle = res;
+		ofn.lpstrInitialDir = dirBuf;
+	}
+	else
+	{
+		ofn.lpstrFileTitle = NULL;
+		ofn.lpstrInitialDir = NULL;
+	}
+
+
+
+
+	// Display the Open dialog box. 
+	
+	if (GetOpenFileNameA(&ofn) == TRUE) {
+
+		int len = strlen(ofn.lpstrFile);
+		if (len > length) {
+			return -1;
+		}
+		strcpy_s(buffer,length, ofn.lpstrFile);
+
+		return 0;
+	}
 
 }
 
-const VCHAR* edv_save_file(edv_file_options* opt) {
+int edv_save_file(const char* lastOpenFile, const char* buffer, size_t length) {
+
+	OPENFILENAMEA ofn;       // common dialog box structure
+	char szFile[260];       // buffer for file name
+	HWND hwnd = 0;              // owner window
+	HANDLE hf;              // file handle
+
+	// Initialize OPENFILENAME
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = hwnd;
+	ofn.lpstrFile = szFile;
+	// Set lpstrFile[0] to '\0' so that GetOpenFileName does not 
+	// use the contents of szFile to initialize itself.
+	ofn.lpstrFile[0] = '\0';
+	ofn.nMaxFile = sizeof(szFile);
+	ofn.lpstrFilter = "All\0*.*\0Text\0*.TXT\0";
+	ofn.nFilterIndex = 1;
+
+	ofn.nMaxFileTitle = 0;
+
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+
+	if (lastOpenFile != NULL) {
+
+		char dirBuf[256];
+		char filenameBuf[256];
+		char extBuf[256];
+
+		//todo - error handling
+		_splitpath_s(lastOpenFile, NULL, 0, dirBuf, 256, filenameBuf, 256, extBuf, 256);
+
+
+		if ((strlen(filenameBuf) + strlen(extBuf)) > 256) {
+			//file buffer too big
+			return NULL;
+		}
+
+		wchar_t* res = strcat(filenameBuf, extBuf);
+
+		ofn.lpstrFileTitle = res;
+		ofn.lpstrInitialDir = dirBuf;
+	}
+	else
+	{
+		ofn.lpstrFileTitle = NULL;
+		ofn.lpstrInitialDir = NULL;
+	}
+
+
+
+
+	// Display the Open dialog box. 
+
+	if (GetSaveFileNameA(&ofn) == TRUE) {
+
+		int len = strlen(ofn.lpstrFile);
+		if (len > length) {
+			return -1;
+		}
+		strcpy_s(buffer, length, ofn.lpstrFile);
+
+		return 0;
+	}
 
 }
 
--- editv/interface.h
+++ editv/interface.h
@@ -2,28 +2,9 @@
 
 #include "SDL3/SDL_video.h"
 
-#ifdef _WIN32
-#define USE_WIDE_CHAR
-#endif
-
-#ifdef USE_WIDE_CHAR
-#include <wchar.h>
-typedef wchar_t VCHAR;
-#else
-typedef char VCHAR;
-#endif
-
-typedef struct {
-	const VCHAR* title;
-	const VCHAR* path;
-	const VCHAR* filter_name;
-	const VCHAR* filter;
-	const VCHAR* extension;
-} edv_file_options;
-
 
 void edv_init(SDL_Window* window);
-const VCHAR* edv_open_file(edv_file_options* opt);
-const VCHAR* edv_save_file(edv_file_options* opt);
+int edv_open_file(const char* lastOpenFile, const char* buffer, size_t length);
+int edv_save_file(const char* lastOpenFile, const char* buffer, size_t length);
 
-const size_t edv_clipboard(char *clip);
+const size_t edv_clipboard(char **clip);
